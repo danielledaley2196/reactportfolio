da4fd07cffb430be241328d6581fba175160764a
Danielle Daley <danielledaley2196@gmail.com>
2020-06-07 21:12:38 -0400

    adding portfolio

---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const Portfolio = () => {
+  return (
+    <Card>
+      <Card.Title>Coding Project</Card.Title>
+    </Card>
+  );
+};
+
+export default Portfolio;
